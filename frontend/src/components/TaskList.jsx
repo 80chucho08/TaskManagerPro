5c2fdcb45f07be61e6bc459ab29e7c543e60f0ff
80chucho08 <chucho2004carbajal@gmail.com>
2025-10-24 20:30:20 -0600

    fix: editar el TaskList para mostrar los datos que tramemos del backend y mostrar las tareas

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from 'react';
+import api from '../api/axiosConfig';
 
 
 function TaskList() {
+    const [task, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const res = await api.get('/tasks');
+                setTasks(res.data);
+            } catch (error) {
+                console.error('Error al obtener tareas: ', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchTasks();
+    }, []);
+
+    if (loading) return <p>Cargando tareas...</p>
+
     return (
         <div>
             <h2>Lista de Tareas</h2>
-            <p>(aqui se mostraran las tareas)</p>
+            {task.length === 0 ? (
+                <p>No hay tareas disponibles.</p>
+            ) : (
+                <ul>
+                    {task.map((task) => (
+                        <li key={task.id}>
+                            {task.title} - {task.completed ? 'Completada' : 'Pendiente'}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }

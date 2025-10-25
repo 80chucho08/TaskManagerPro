c533de3e50387ce2336b870b8170958b0dca558a
80chucho08 <chucho2004carbajal@gmail.com>
2025-10-24 19:37:39 -0600

    feat: creación de los componentes basicos para mostrar tareas y formulario para introducir nuevas tareas

---
+++ frontend/src/components/TaskForm.jsx
@@ -0,0 +1,12 @@
+
+
+function TaskForm () {
+    return (
+        <div>
+            <h3>Formulario de nueva tarea</h3>
+            <p>(Aquí irá el input boton para agregar tareas)</p>
+        </div>
+    )
+}
+
+export default TaskForm;;
---
+++ frontend/src/components/TaskList.jsx
@@ -0,0 +1,12 @@
+
+
+function TaskList() {
+    return (
+        <div>
+            <h2>Lista de Tareas</h2>
+            <p>(aqui se mostraran las tareas)</p>
+        </div>
+    )
+}
+
+export default TaskList;

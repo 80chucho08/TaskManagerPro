58f52ed88dffab45e61e77d2ce21465bf22abc4f
80chucho08 <chucho2004carbajal@gmail.com>
2025-11-09 21:36:57 -0600

    style: Estilado con tailwind a la lista de tareas mostrando cada tarea con una card, y estilando tanto el check de completed y el boton de eliminar

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,27 +1,40 @@
-import { useState } from 'react';
-
 
 function TaskList({ tasks, onDelete, onChecked }) {
   return (
     <div>
-      <h2>Lista de tareas</h2>
+      <h2 className="text-xl font-semibold mb-4">Lista de tareas</h2>
       {tasks.length === 0 ? (
-        <p>No hay tareas registradas.</p>
+        <p className="text-gray-500">No hay tareas registradas.</p>
       ) : (
-        <ul>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {tasks.map((task) => (
-            <li key={task.id}>
-              {task.title} — {task.completed ? "Completado" : "Pendiente"}
-              <button onClick={() => onDelete(task.id)} >Eliminar</button>
-              <input
-                type="checkbox"
-                checked={task.completed === 1 || task.completed === true}
-                onChange={() => onChecked(task.id, task.completed)}
-              />
-
-            </li>
+            <div key={task.id} className="bg-white p-4 rounded-xl shadow-md flex flex-col justify-between">
+              <div>
+                <h3 className="font-bold text-lg text-gray-800">{task.title}</h3>
+                <p className="text-gray-600">{task.description}</p>
+              </div>
+              <div className="flex items-center justify-between mt-4">
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => onChecked(task.id, task.completed)}
+                    className="mr-2"
+                  />
+                  <span className={task.completed ? "text-green-500" : "text-red-500"}>
+                    {task.completed ? 'Completado' : 'Pendiente'}
+                  </span>
+                </div>
+                <button
+                  onClick={() => onDelete(task.id)}
+                  className="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600 transition"
+                  >
+                    Eliminar
+                </button>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
